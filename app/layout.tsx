ff19cc497066638aeba6da9930ca38f36fd53f1f
yousrakhan23 <ammarahkhan2016@gmail.com>
2024-10-17 22:40:40 +0500

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
