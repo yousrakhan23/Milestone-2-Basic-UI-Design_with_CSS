888159dec266d8b5bc81ad5e1a88d6e93b44466f
yousrakhan23 <ammarahkhan2016@gmail.com>
2024-10-18 20:09:46 +0500

    video

--- app/AboutPage/page.tsx
+++ app/AboutPage/page.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import video1 from '@/app/assets/videos/video1'
+
+
 
 const AboutPage = () => {
   return (
     <>
       <div>
-        <h1>AboutPage</h1>
+        <h1>HEXA SHIELD PRODUCTS</h1>
+        <div className="w-full max-w-4xl aspect-video overflow-hidden rounded-lg shadow-lg">
+          <iframe className="w-full h-full" 
+          src="/assets/videos/video.mp4"/>
+        </div>
         <p>This is the about page.</p>
         
       </div>
